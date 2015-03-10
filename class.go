3bd6ca03f7b54dc6b686ab0616f81b865126bba5
Trent Summerfield <trent.summerfield@gmail.com>
2015-03-10 17:41:12 +1100

    Read the constant pool from a class file

--- class.go
+++ class.go
@@ -3,12 +3,67 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 )
 
 type Class struct {
-	magic                      uint32
-	minorVersion, majorVersion uint16
-	constantPoolCount          uint16
+	magic             uint32
+	minorVersion      uint16
+	majorVersion      uint16
+	constantPoolCount uint16
+	constantPoolItems []interface{}
+}
+
+type NameAndType struct {
+	NameIndex       uint16
+	DescriptorIndex uint16
+}
+
+func (m NameAndType) String() string {
+	return fmt.Sprintf("NameAndType: %v, %v", m.NameIndex, m.DescriptorIndex)
+}
+
+type UTF8String struct {
+	Length uint16
+	Bytes  []uint8
+}
+
+func (m UTF8String) String() string {
+	return fmt.Sprintf("UTF8: %v", string(m.Bytes))
+}
+
+type ClassInfo struct {
+	NameIndex uint16
+}
+
+func (m ClassInfo) String() string {
+	return fmt.Sprintf("Class: %v", m.NameIndex)
+}
+
+type MethodRef struct {
+	ClassIndex       uint16
+	NameAndTypeIndex uint16
+}
+
+func (m MethodRef) String() string {
+	return fmt.Sprintf("Method: %v, %v", m.ClassIndex, m.NameAndTypeIndex)
+}
+
+type FieldRef struct {
+	ClassIndex       uint16
+	NameAndTypeIndex uint16
+}
+
+func (m FieldRef) String() string {
+	return fmt.Sprintf("Field: %v, %v", m.ClassIndex, m.NameAndTypeIndex)
+}
+
+type StringConstant struct {
+	UTF8Index uint16
+}
+
+func (m StringConstant) String() string {
+	return fmt.Sprintf("String: %v", m.UTF8Index)
 }
 
 type ParseError struct {
@@ -19,31 +74,93 @@ func (e ParseError) Error() string {
 	return e.reason
 }
 
-func parse(b []byte) (Class, error) {
-	var magic uint32
-	var minorVersion, majorVersion uint16
-	var constantPoolCount uint16
+func parseMethodRef(buf *bytes.Reader) (m MethodRef, err error) {
+	err = binary.Read(buf, binary.BigEndian, &m)
+	return
+}
+
+func parseFieldRef(buf *bytes.Reader) (m FieldRef, err error) {
+	err = binary.Read(buf, binary.BigEndian, &m)
+	return
+}
+
+func parseStringConstant(buf *bytes.Reader) (s StringConstant, err error) {
+	err = binary.Read(buf, binary.BigEndian, &s)
+	return
+}
+
+func parseClassInfo(buf *bytes.Reader) (c ClassInfo, err error) {
+	err = binary.Read(buf, binary.BigEndian, &c)
+	return
+}
+
+func parseNameAndType(buf *bytes.Reader) (c NameAndType, err error) {
+	err = binary.Read(buf, binary.BigEndian, &c)
+	return
+}
+
+func parseUTF8String(buf *bytes.Reader) (c UTF8String, err error) {
+	err = binary.Read(buf, binary.BigEndian, &c.Length)
+	if err != nil {
+		return
+	}
+	c.Bytes = make([]byte, c.Length)
+	var i uint16
+	for i = 0; i < c.Length; i++ {
+		c.Bytes[i], err = buf.ReadByte()
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func parseConstantPoolItem(buf *bytes.Reader) (interface{}, error) {
+	tag, err := buf.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	if tag == 10 {
+		return parseMethodRef(buf)
+	} else if tag == 9 {
+		return parseFieldRef(buf)
+	} else if tag == 8 {
+		return parseStringConstant(buf)
+	} else if tag == 7 {
+		return parseClassInfo(buf)
+	} else if tag == 1 {
+		return parseUTF8String(buf)
+	} else if tag == 12 {
+		return parseNameAndType(buf)
+	}
+	return tag, nil
+}
+
+func parse(b []byte) (c Class, err error) {
 	buf := bytes.NewReader(b)
-	err := binary.Read(buf, binary.BigEndian, &magic)
+	err = binary.Read(buf, binary.BigEndian, &c.magic)
 	if err != nil {
-		return Class{}, err
+		return
 	}
-	err = binary.Read(buf, binary.BigEndian, &minorVersion)
+	err = binary.Read(buf, binary.BigEndian, &c.minorVersion)
 	if err != nil {
-		return Class{}, err
+		return
 	}
-	err = binary.Read(buf, binary.BigEndian, &majorVersion)
+	err = binary.Read(buf, binary.BigEndian, &c.majorVersion)
 	if err != nil {
-		return Class{}, err
+		return
 	}
-	err = binary.Read(buf, binary.BigEndian, &constantPoolCount)
+	err = binary.Read(buf, binary.BigEndian, &c.constantPoolCount)
 	if err != nil {
-		return Class{}, err
+		return
 	}
-	return Class{
-		magic:             magic,
-		minorVersion:      minorVersion,
-		majorVersion:      majorVersion,
-		constantPoolCount: constantPoolCount,
-	}, nil
+	c.constantPoolItems = make([]interface{}, c.constantPoolCount)
+	var i uint16
+	for i = 0; i < c.constantPoolCount; i++ {
+		c.constantPoolItems[i], err = parseConstantPoolItem(buf)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
--- class_test.go
+++ class_test.go
@@ -3,27 +3,61 @@ package main
 import (
 	"io/ioutil"
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
-func TestFailure(t *testing.T) {
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type ClassSuite struct {
+	class Class
+}
+
+var _ = Suite(&ClassSuite{})
+
+func (s *ClassSuite) SetUpSuite(c *C) {
 	bytes, err := ioutil.ReadFile("Hello.class")
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-	class, err := parse(bytes)
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-	if class.magic != 0xCAFEBABE {
-		t.Errorf("Magic number is wrong. Expected 0xCAFEBABE but got 0x%X", class.magic)
-	}
-	if class.minorVersion != 0 {
-		t.Errorf("Minor number is wrong. Expected 0 but got %d", class.minorVersion)
-	}
-	if class.majorVersion != 52 {
-		t.Errorf("Major number is wrong. Expected 52 but got %d", class.majorVersion)
-	}
-	if class.constantPoolCount != 29 {
-		t.Errorf("Constant pool count is wrong. Expected 29 but got %d", class.constantPoolCount)
-	}
+	c.Assert(err, IsNil)
+	s.class, err = parse(bytes)
+	c.Assert(err, IsNil)
+}
+
+func (s *ClassSuite) TestClassMagicAndVersions(c *C) {
+	c.Check(s.class.magic, Equals, uint32(0xCAFEBABE))
+	c.Check(s.class.majorVersion, Equals, uint16(52))
+	c.Check(s.class.minorVersion, Equals, uint16(0))
+}
+
+func (s *ClassSuite) TestClassConstantPool(c *C) {
+	c.Check(s.class.constantPoolCount, Equals, uint16(29))
+	c.Check(s.class.constantPoolItems[0], FitsTypeOf, MethodRef{})
+	c.Check(s.class.constantPoolItems[1], FitsTypeOf, FieldRef{})
+	c.Check(s.class.constantPoolItems[2], FitsTypeOf, StringConstant{})
+	c.Check(s.class.constantPoolItems[3], FitsTypeOf, MethodRef{})
+	c.Check(s.class.constantPoolItems[4], FitsTypeOf, ClassInfo{})
+	c.Check(s.class.constantPoolItems[5], FitsTypeOf, ClassInfo{})
+	c.Check(s.class.constantPoolItems[6], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[7], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[8], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[9], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[10], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[11], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[12], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[13], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[14], FitsTypeOf, NameAndType{})
+	c.Check(s.class.constantPoolItems[15], FitsTypeOf, ClassInfo{})
+	c.Check(s.class.constantPoolItems[16], FitsTypeOf, NameAndType{})
+	c.Check(s.class.constantPoolItems[17], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[18], FitsTypeOf, ClassInfo{})
+	c.Check(s.class.constantPoolItems[19], FitsTypeOf, NameAndType{})
+	c.Check(s.class.constantPoolItems[20], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[21], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[22], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[23], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[24], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[25], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[26], FitsTypeOf, UTF8String{})
+	c.Check(s.class.constantPoolItems[27], FitsTypeOf, UTF8String{})
 }
