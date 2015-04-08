5b4d35207313da41378d8a33e25b2835dfd2f60e
Trent Summerfield <trent.summerfield@gmail.com>
2015-04-08 13:07:50 +1000

    added integer division

--- tests/IntegerArithmetic/IntegerArithmetic.java
+++ tests/IntegerArithmetic/IntegerArithmetic.java
@@ -4,6 +4,7 @@ public class IntegerArithmetic {
 		printInt(minus(30, 2));
 		printInt(minus(55, 60));
 		printInt(mult(2, 50));
+		printInt(div(20, 10));
 	}
 
 	public static int add(int x, int y) {
@@ -16,6 +17,10 @@ public class IntegerArithmetic {
 
 	public static int mult(int x, int y) {
 		return x * y;
+	}
+
+	public static int div(int x, int y) {
+		return x / y;
 	}
 
 	public static native void printInt(int x);
